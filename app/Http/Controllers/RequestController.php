8e633d262fed6473b7e15a984cf95c511ce0a36c
armia-roma <armia_bos@gmail.com>
2025-07-16 22:41:54 +0400

    feat: add API endpoint and controller for creating towing requests

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\TowingRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class RequestController extends Controller
+{
+    public function store(Request $request) {}
+}
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,11 @@
 <?php
 
+use App\Http\Controllers\RequestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('/posts', function () {
-    return response()->json([
-        ['id' => 1, 'title' => 'First Post', 'content' => 'This is the content of the first post.'],
-        ['id' => 2, 'title' => 'Second Post', 'content' => 'This is the content of the second post.'],
-    ]);
-});
+
+Route::post('/requests', [RequestController::class, 'store']);
