e90bc47371e71f39feadb470c7674f7ff5ae1e1b
armia-roma <armia_bos@gmail.com>
2025-07-17 00:21:29 +0400

    feat: add global validation exception handling in app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use Illuminate\Validation\ValidationException;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,5 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        // Handle validation exceptions globally
+        $exceptions->render(function (ValidationException $e, $request) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Validation failed',
+            'errors' => $e->errors(), // field errors
+        ], 422);
+    });
     })->create();
