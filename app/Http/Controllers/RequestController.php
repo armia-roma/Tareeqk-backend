bcf8c2a3dd90a1b72965f022c37b5432b465bdae
armia-roma <armia_bos@gmail.com>
2025-07-16 23:41:20 +0400

    refactor: update store method use StoreTowingRequest for validation

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -3,29 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTowingRequest;
 use App\Models\TowingRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Services\TowingRequestService;
+
 class RequestController extends Controller
 {
     protected $towingRequestService;
     public function __construct(TowingRequestService $towingRequestService)
-{
-    $this->towingRequestService = $towingRequestService;
-}
-    public function store(Request $request)
+    {
+        $this->towingRequestService = $towingRequestService;
+    }
+    public function store(StoreTowingRequest $request): JsonResponse
     {
         try {
-
-            $validated = $request->validate([
-                'customer_id' => 'required|integer',
-                'lat' => 'required|numeric|between:-90,90',
-                'lang' => 'required|numeric|between:-180,180',
-                'note' => 'nullable|string|max:1000',
-            ]);
-
-            $towingRequest = $this->towingRequestService->create($validated);
+            $towingRequest = $this->towingRequestService->create($request->validated());
 
             return response()->json([
                 'success' => true,
--- app/Http/Requests/StoreTowingRequest.php
+++ app/Http/Requests/StoreTowingRequest.php
@@ -11,7 +11,7 @@ class StoreTowingRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
