40af24dc4559a519120ae73521fc34223e864e07
armia-roma <armia_bos@gmail.com>
2025-07-18 12:12:44 +0400

    feat: add unique email validation to RegisterRequest

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -24,7 +24,7 @@ class RegisterRequest extends FormRequest
         return [
             //
             'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
+            'email' => 'required|email|max:255|unique:users,email',
             'password' => 'required|string|min:6',
         ];
     }
